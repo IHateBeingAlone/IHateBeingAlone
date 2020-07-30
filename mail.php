4e8bc1da34e1e3c5e3a7bd27e2059cd7b85d7456
IHateBeingAlone <40752929+IHateBeingAlone@users.noreply.github.com>
2020-07-30 18:29:41 +0600

    123

--- mail.php
+++ mail.php
@@ -13,13 +13,13 @@ $email = $_POST['user_email'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'aroww200@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '19072009ovecded1907200928461478963'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'test1pochta@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '19072009ovecded'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('aroww200@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('aroww200@gmail.com');     // Кому будет уходить письмо 
+$mail->setFrom('test1pochta@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('test2pochta@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
